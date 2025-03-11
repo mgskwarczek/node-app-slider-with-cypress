449e42b53ffc17d9426c386922c4a7731fb88ae4
Magdalena Skwarczek <magdalena.skwarczek@stazysta.comarch.pl>
2025-03-11 22:22:58 +0100

    6

--- public/script.js
+++ public/script.js
@@ -1,15 +1,21 @@
 const swiper = new Swiper(".swiper", {
-    loop: true,
-    effect: "fade",
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-    },
-    pagination: {
-        el: ".swiper-pagination"
-    },
-    keyboard: {
-        enabled: true,
-        onlyInViewport: false
-    }
-}); 
+  loop: true,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+injectContentsquareScript({
+  siteId: "5334322", // Twój siteId
+  async: true, // Opcjonalnie: ustaw na `false`, jeśli chcesz poczekać na załadowanie dokumentu przed uruchomieniem skryptu
+  defer: false, // Opcjonalnie: ustaw na `true`, jeśli chcesz, żeby skrypt załadował się po załadowaniu strony
+});
